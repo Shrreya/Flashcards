7a65a68f8e5bdeba7407ccf2191e73645c42281c
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-23 20:56:59 +0530

    Added stack navigator with home screen

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { Constants } from 'expo';
 import DeckList from './components/DeckList';
-import { green } from './utils/colors';
+import { white, green } from './utils/colors';
+import { createStackNavigator } from 'react-navigation';
 
 function MyStatusBar ({backgroundColor, ...props}) {
   return (
@@ -12,12 +13,25 @@ function MyStatusBar ({backgroundColor, ...props}) {
   )
 }
 
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: DeckList,
+    navigationOptions: {
+      title: 'FLASHCARDS',
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: green,
+      }
+    }
+  }
+});
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={{flex:1}}>
         <MyStatusBar backgroundColor={green} barStyle="light-content" />
-        <DeckList/>
+        <AppNavigator/>
       </View>
     );
   }
--- utils/colors.js
+++ utils/colors.js
@@ -1,1 +1,2 @@
 export const green = '#00897B';
+export const white = '#fff';
