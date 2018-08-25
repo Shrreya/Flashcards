744c5a296a096bea8210a8dff3eec1204b8717e6
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-26 03:47:02 +0530

    Deck Preview Component

---
+++ components/DeckPreview.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { green, lightGrey, darkGrey } from '../utils/colors';
+import { getDeckPreviewSizes } from '../utils/helpers';
+
+export default function DeckPreview ({ deck }) {
+
+  const title = deck[Object.keys(deck)[0]].title;
+  const cardCount = deck[Object.keys(deck)[0]].questions.length;
+
+  return (
+    <View style={styles.deckPreview}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.cardCount}>{cardCount} cards</Text>
+    </View>
+  );
+}
+
+const { width, height, margin } = getDeckPreviewSizes();
+
+const styles = StyleSheet.create({
+  deckPreview : {
+    width: width,
+    height: height,
+    borderWidth: 1,
+    borderColor: darkGrey,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginLeft: margin,
+    marginRight: margin
+  },
+  title : {
+    fontSize : 20,
+    color : green
+  },
+  cardCount : {
+    color : lightGrey
+  }
+});
--- utils/colors.js
+++ utils/colors.js
@@ -1,2 +1,4 @@
 export const green = '#00897B';
 export const white = '#fff';
+export const lightGrey = '#969696';
+export const darkGrey = '#BDBDBD';
