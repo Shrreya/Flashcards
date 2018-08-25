8662d7d1520bce1577c65bea9006ed6bbacba79f
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-25 17:13:24 +0530

    Added reducers

---
+++ reducers/index.js
@@ -0,0 +1,23 @@
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD_TO_DECK } from '../actions';
+
+export default function decks (state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_DECKS :
+      return {
+        ...state,
+        ...action.decks,
+      }
+    case ADD_DECK :
+      return {
+        ...state,
+        ...action.deck
+      }
+    case ADD_CARD_TO_DECK :
+      return {
+        ...state,
+        //TODO: add card to deck
+      }
+    default :
+      return state
+  }
+}
