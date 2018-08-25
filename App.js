384c435fe1fe755fc6e84b1ebe777d200c554391
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-25 17:17:01 +0530

    Setup Redux

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 import { View } from 'react-native';
 import MyStatusBar from './components/MyStatusBar';
 import AppNavigator from './components/AppNavigator';
@@ -6,10 +9,12 @@ import AppNavigator from './components/AppNavigator';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex:1}}>
-        <MyStatusBar/>
-        <AppNavigator/>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{flex:1}}>
+          <MyStatusBar/>
+          <AppNavigator/>
+        </View>
+      </Provider>
     );
   }
 }
