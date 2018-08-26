bb7a60aaf49c2803902d934963bac295b643d781
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-27 03:21:34 +0530

    Completed deck component

--- components/Deck.js
+++ components/Deck.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { green, lightGrey, white } from '../utils/colors';
 
 class Deck extends Component {
 
@@ -7,15 +9,73 @@ class Deck extends Component {
     title: navigation.state.params.title
   });
 
+  addCard = () => {
+    // TODO : route to add card screen
+  };
+
+  startQuiz = () => {
+    // TODO : route to quiz screen
+  }
+
   render() {
+
+    const { deck } = this.props;
+
     return (
-      <View>
-        <Text>
-          Deck here
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          {deck.title}
         </Text>
+        <Text style={styles.cardCount}>
+          {deck.questions.length} cards
+        </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.addCard}>
+          <Text style={styles.buttonText}>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.startQuiz}>
+          <Text style={styles.buttonText}>Start Quiz</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-export default Deck;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 30,
+    color: green
+  },
+  cardCount: {
+    fontSize: 20,
+    color: lightGrey
+  },
+  button: {
+    height: 45,
+    backgroundColor: green,
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 5,
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: white,
+    textAlign: 'center'
+  }
+});
+
+const mapStateToProps = (decks, { navigation }) => {
+  const { title } = navigation.state.params;
+  return { deck: decks[title] };
+}
+
+export default connect(mapStateToProps)(Deck);
