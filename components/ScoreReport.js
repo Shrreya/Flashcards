216359882c502f61d1ce564934d1f32fa340ecba
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-29 01:28:28 +0530

    Completed score report component

--- components/ScoreReport.js
+++ components/ScoreReport.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { green, white } from '../utils/colors';
 
 export default function ScoreReport ({ navigation }) {
 
   const { score, total, title } = navigation.state.params;
 
   return (
-    <View>
-      <Text>
-        Score - {score} out of {total} for {title}
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        {title}
       </Text>
+      <Text style={styles.score}>
+        You scored {score} out of {total}
+      </Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.push('Quiz', { title })}>
+        <Text style={styles.buttonText}>Restart Quiz</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Deck', { title })}>
+        <Text style={styles.buttonText}>Back to Deck</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 30,
+    color: green,
+    padding: 10
+  },
+  score: {
+    fontSize: 20,
+    padding: 10
+  },
+  button: {
+    height: 45,
+    backgroundColor: green,
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 5,
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: white,
+    textAlign: 'center'
+  }
+});
