bf5ee407b1b11626cba5089b92490e9d44a3b91a
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-29 02:11:59 +0530

    API to create & set daily notification

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,9 @@
 import { AsyncStorage } from 'react-native';
+import { Notifications, Permissions } from 'expo';
+import { createNotification } from './helpers';
 
 const DECKS_STORAGE_KEY = 'Flashcards:decks';
+const NOTIFICATION_KEY = 'Flashcards:notifications';
 
 export function fetchDecks () {
   return AsyncStorage.getItem(DECKS_STORAGE_KEY);
@@ -19,4 +22,39 @@ export function submitCardToDeck (card, title) {
       decks[title].questions = decks[title].questions.concat(card);
       AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(decks));
     });
+}
+
+export function clearLocalNotification () {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync);
+}
+
+export function setLocalNotification () {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync();
+
+              let tomorrow = new Date();
+              tomorrow.setDate(tomorrow.getDate() + 1);
+              tomorrow.setHours(20);
+              tomorrow.setMinutes(0);
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              );
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+            }
+          });
+      }
+  });
 }
