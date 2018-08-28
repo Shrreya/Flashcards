0687c19c40e7b91147607e1fa2a3e1055173fbcf
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-28 17:30:05 +0530

    Added score report screen to navigator

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -3,6 +3,7 @@ import DeckList from './DeckList';
 import Deck from './Deck';
 import AddCard from './AddCard';
 import Quiz from './Quiz';
+import ScoreReport from './ScoreReport';
 import { white, green } from '../utils/colors';
 
 const AppNavigator = createStackNavigator({
@@ -39,6 +40,16 @@ const AppNavigator = createStackNavigator({
     screen: Quiz,
     navigationOptions: {
       title: 'Quiz',
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: green,
+      }
+    }
+  },
+  ScoreReport: {
+    screen: ScoreReport,
+    navigationOptions: {
+      title: 'Score Report',
       headerTintColor: white,
       headerStyle: {
         backgroundColor: green,
